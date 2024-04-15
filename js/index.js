edf99e357ab8f9d22a01509be4d60b126df08d5e
devravindu <hello.devrv@gmail.com>
2024-04-15 22:05:54 +0530

    Update Home Page

--- js/index.js
+++ js/index.js
@@ -13,28 +13,6 @@ if (close) {
   })
 }
 
-// Kokis Animation
-/*
-let container = document.getElementById('container');
-let count = 50;
-
-for (let i = 0; i < 30; i++) {
-let leftKokis = Math.floor(Math.random() * container.clientWidth);
-let topKokis = Math.floor(Math.random() * container.clientHeight);
-let widthKokis = Math.floor(Math.random() * 50);
-  console.log(leftKokis);
-  let timeKokis = Math.floor(Math.random() * 5) + 5;
-  let div = document.createElement('div');
-  div.classList.add('kokis');
-  div.style.left = leftKokis + 'px';
-  div.style.top = topKokis + 'px';
-  div.style.width = widthKokis + 'px';
-  div.style.height = widthKokis + 'px';
-  div.style.animationDuration = 1 + timeKokis + 's';
-  container.appendChild(div);
-}
-*/
-
 
 // Single Product Page Image Carrousoul
 

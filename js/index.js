025916adc53a4a1e31a534dfa184ab4b416be352
devravindu <hello.devrv@gmail.com>
2024-05-05 23:09:26 +0530

    Refactor code for responsive navigation bar

--- js/index.js
+++ js/index.js
@@ -1,36 +1,30 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-const close = document.getElementById('close');
+const sideNavBar = document.getElementById("side-nav-bar");
 
-if (bar) {
-  bar.addEventListener('click', () => {
-    nav.classList.add('active');
-  })
-}
-if (close) {
-  close.addEventListener('click', () => {
-    nav.classList.remove('active');
-  })
+function showSideNavBar() {
+  sideNavBar.style.display = "flex";
 }
 
+function hideSideNavBar() {
+  sideNavBar.style.display = "none";
+}
 
 // Single Product Page Image Carrousoul
 
-var productImage = document.getElementById('proImage');
-var smallImages = document.getElementsByClassName('gallery-img');
+var productImage = document.getElementById("proImage");
+var smallImages = document.getElementsByClassName("gallery-img");
 
 for (let i = 0; i < smallImages.length; i++) {
-  smallImages[i].addEventListener('click', function() {
+  smallImages[i].addEventListener("click", function () {
     productImage.src = this.src;
   });
 }
 
 // Get all .card elements inside #product
-let cards = document.querySelectorAll('#product .card');
+let cards = document.querySelectorAll("#product .card");
 
 // Add event listener to each card
-cards.forEach(card => {
-    card.addEventListener('click', function() {
-        window.location.href = 'single-product.html';
-    });
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    window.location.href = "single-product.html";
+  });
 });
